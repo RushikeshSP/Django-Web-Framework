76056a9c6fdb18314195dba0fa1a6499464e5835
Rushikesh <Rushikeshpokharkar2001@gmail.com>
2021-09-19 13:53:51 +0530

    Customized the Django admin interface.

--- My Portfolio/portfolio/home/urls.py
+++ My Portfolio/portfolio/home/urls.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from home import views
 
+
+# django Admin header customization.
+
+admin.site.site_header = "Developer Rushi"
+admin.site.site_title = "Welcome to Rushi's Dashboard"
+admin.site.index_title = "Welcome to the Portal"
+ 
+
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
