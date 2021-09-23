f4fd4d1e480750479672ae364c6edb412c81190a
Rushikesh <Rushikeshpokharkar2001@gmail.com>
2021-09-23 12:11:09 +0530

    Updated the todoList to add backend database into it.

--- TODO List/todoList/home/views.py
+++ TODO List/todoList/home/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Task
 
 # Create your views here.
 def home(request):
+    if (request.method == "POST"):
+        task_name = request.POST['task_name']
+        date = request.POST['date']
+        desc = request.POST['desc']
+
+        ins = Task(task_name = task_name, date = date, desc = desc)
+        ins.save()
+        print("Data is save into the database.")
+
     return render(request, "home.html")
 
     
