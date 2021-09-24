d2353a89a1dffd80c09536adc39a6b1284b66cc5
Rushikesh <Rushikeshpokharkar2001@gmail.com>
2021-09-24 16:20:44 +0530

    created a model and predicted the output.

--- Forest Fire Prevention Using Django/forestFirePrevention/home/views.py
+++ Forest Fire Prevention Using Django/forestFirePrevention/home/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
 import templates
+import pickle
+import numpy as np
 
 # Create your views here.
 
 def home(request):
+    if(request.method == "POST"):
+        oxygen = request.POST['oxygen']
+        temperature = request.POST['temperature']
+        humidity = request.POST['humidity']
+
+
+
+        model = pickle.load(open("model.pkl", "rb"))
+
+        features = [int(oxygen), int(temperature), int(humidity)]
+        final = [np.array(features)]
+
+        prediction = model.predict_proba(final)
+        output = '{0:.{1}f}'.format(prediction[0][1], 2)
+
+        if output>str(0.5):
+            print('Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output))
+        else:
+            print('Your Forest is safe.\n Probability of fire occuring is {}'.format(output))
+
+        # context = {'oxygen': oxygen, 'temperature': temperature, 'humidity': humidity, 'output': output}
+        # return render(request, 'base.html', context)
+
     return render(request, 'base.html')
 
 
