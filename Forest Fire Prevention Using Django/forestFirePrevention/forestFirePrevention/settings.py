eecee4134731330822ff0e2085b5174c8f3426a6
Rushikesh <Rushikeshpokharkar2001@gmail.com>
2021-10-01 21:12:47 +0530

    Added an .env file to store the SECRET_KEY for sefty.

--- Forest Fire Prevention Using Django/forestFirePrevention/forestFirePrevention/settings.py
+++ Forest Fire Prevention Using Django/forestFirePrevention/forestFirePrevention/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hr-p#%eb%@=f&sqky1==f6b7++el%hl-5e5w)ouma213*(y38^'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
